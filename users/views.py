74e4d7416f8f135ea1501a96455f2f79ab0c2479
islamelsayed <110628450+ielsayed-dev@users.noreply.github.com>
2026-01-05 00:15:11 +0200

    fixed permissions

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]  # Add this line
